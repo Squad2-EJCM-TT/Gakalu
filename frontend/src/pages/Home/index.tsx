50211fbb468b5bd21f191efe058a702098a7e5d5
victorpss <victorpss@dcc.ufrj.br>
2024-08-07 15:51:51 -0300

    refactor: changed components repetition to .map()

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,19 +6,36 @@ import Footer from "../../components/Footer";
 import Product from "../../components/Product";
 
 export default function Home(){
+
+    const categories = [
+        { index: 1, title: "Jogos Eletrônicos" },
+        { index: 2, link: "", title: "Peças de Computadores" },
+        { index: 3, title: "Acessórios Novos" },
+        { index: 4, title: "Acessórios Semi-Novos" },
+    ];
+
+    const products = [
+        { visible: true, index: 1, title: "The Legend of Zelda: Breath of the Wild (Nintendo Switch)", location: "Rio de Janeiro", price: "R$ 98.99" },
+        { visible: true, index: 2, title: "COOLER DEEPCOOL PARA AMD CK-AM209, DP-ACAL-A09", location: "Minas Gerais", price: "R$ 25.98" },
+        { visible: true, index: 3, title: "HEADSET GAMER MULTI WARRIOR", location: "São Paulo", price: "R$ 83.99" },
+        { visible: true, index: 4, title: "FONE DE OUVIDO MULTI HEADPHONE POP PRETO PH053", location: "Acre", price: "R$ 31.98" },
+    ];
+
     return (
         <>
+            <Header/>
             <Container>
-                <Header/>
                 <Banner>
                     <BannerImage source={require("../../assets/banner/bannerImage1.png")}></BannerImage>
                     <BannerImage source={require("../../assets/banner/bannerImage2.png")}></BannerImage>
                 </Banner>
                 <Categories>
-                    <Category index={1} title="Jogos Eletrônicos"/> 
-                    <Category index={2} link="" title="Peças de Computadores"/> {/* adicionar link p/ tela de peças */}
-                    <Category index={3} title="Acessórios Novos"/>
-                    <Category index={4} title="Acessórios Semi-Novos"/>
+                    {categories.map((category) => (
+                        <Category
+                            index={category.index}
+                            link={category.link}
+                            title={category.title}/>
+                    ))}
                 </Categories>
                 <SearchContainer>
                     <Lupa source={require("../../assets/icons/lupa.png")}></Lupa>
@@ -30,10 +47,15 @@ export default function Home(){
                 </OffersContainer>
                 <Linha></Linha>
                 <ProductsDisplay>
-                    <Product visible={true} index={1} title="The Legend of Zelda: Breath of the Wild (Nintendo Switch)" location="Rio de Janeiro" price="R$ 98.99"/>
-                    <Product visible={true} index={2} title="COOLER DEEPCOOL PARA AMD CK-AM209, DP-ACAL-A09" location="Minas Gerais" price="R$ 25.98"/>
-                    <Product visible={true} index={3} title="HEADSET GAMER MULTI WARRIOR " location="São Paulo" price="R$ 83.99"/>
-                    <Product visible={true} index={4} title="FONE DE OUVIDO MULTI HEADPHONE POP PRETO PH053" location="Acre" price="R$ 31.98"/>
+                    {products.map((product) => (
+                        <Product 
+                            visible={product.visible}
+                            index={product.index}
+                            title={product.title}
+                            location={product.location}
+                            price={product.price}
+                        />
+                    ))}
                 </ProductsDisplay>
             </Container>
             <Footer/>
