85fb822ec59e86f0feb57f89aeb67f96633ce601
victorpss <victorpss@dcc.ufrj.br>
2024-08-08 15:51:43 -0300

    feature: routes

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/pages/Home';
 import Products from './src/pages/Products';
+import Route from './src/routes';
 
 export default function App() {
   return (
-    <Home/>
+    <Route/>
     //<Products/>
   );
 }
